d359a480e1c0596928e9dc1168bfd29a3353f88e
Fazil <mfazilkp7@gmail.com>
2023-09-26 14:31:31 +0530

    LifeMate Project 1st commit

---
+++ android/app/src/main/kotlin/com/example/lifemate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lifemate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/User/user_login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserLoginPage extends StatefulWidget {
+  const UserLoginPage({super.key});
+
+  @override
+  State<UserLoginPage> createState() => _UserLoginPageState();
+}
+
+class _UserLoginPageState extends State<UserLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
