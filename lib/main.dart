a9539d80ba2b397cb1345ad03fd21db1e0ccabe7
Fazil <mfazilkp7@gmail.com>
2023-10-07 14:30:42 +0530

    admin page updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lifemate/ui/User/user_login_page.dart';
-import 'package:lifemate/ui/User/user_splash_screen.dart';
+import 'package:lifemate/splash_screen.dart';
 
 import 'firebase/firebase_options.dart';
 
